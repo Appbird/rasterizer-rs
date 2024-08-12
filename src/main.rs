3e5f293c6fcf9847a46494bbb6f28819feeb1e07
appbird <project.appbird@gmail.com>
2024-08-12 18:28:34 +0900

    Hello Window!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use fltk::app;
+use fltk::prelude::{GroupExt, WidgetExt};
+use fltk::window::Window;
+const WIDTH:u32 = 640;
+const HEIGHT:u32 = 480;
+
 fn main() {
-    println!("Hello, world!");
+    let app = app::App::default();
+    let mut win = Window::default()
+        .with_size(WIDTH as i32, HEIGHT as i32)
+        .with_label("Hello Pixels");
+    win.make_resizable(true);
+    win.end();
+    win.show();
+
+    app.run().unwrap();
+    
 }
