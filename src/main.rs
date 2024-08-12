af867de0a49271e897f7c5a5deb8df3f2bbe6671
appbird <project.appbird@gmail.com>
2024-08-13 01:27:26 +0900

    draw points

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,45 @@
-use log::error;
-use error_iter::ErrorIter as _;
-use fltk::app;
-use fltk::prelude::*;
-use fltk::window::Window;
-use pixels::{Error, Pixels, SurfaceTexture};
-const WIDTH:u32 = 640;
-const HEIGHT:u32 = 480;
+use minifb::{Key, Window, WindowOptions};
+const WIDTH: usize = 640;
+const HEIGHT: usize = 480;
 
-fn sq(x:i32) -> i32 { x * x }
+struct Vec3 {
+    e:[f64;3]
+}
+impl Vec3 {
+    pub fn x(&self) -> f64 { self.e[0] }
+    pub fn y(&self) -> f64 { self.e[1] }
+    pub fn z(&self) -> f64 { self.e[2] }
+    pub fn new(x:f64, y:f64, z:f64) -> Vec3 { Vec3{ e:[x, y, z] } }
+}
+type Color = Vec3;
 
-fn main() -> Result<(), Error> {
-    let app = app::App::default();
-    let mut win = Window::default()
-        .with_size(WIDTH as i32, HEIGHT as i32)
-        .with_label("Hello Pixels");
-    win.make_resizable(true);
-    win.end();
-    win.show();
+fn encode_color(color:Color) -> u32 {
+    let r = (color.x() * 255.999).floor() as u32;
+    let g = (color.y() * 255.999).floor() as u32;
+    let b = (color.z() * 255.999).floor() as u32;
+    return (r << 8 * 2) | (g << 8 * 1) | (b << 8 * 0); 
+}
+fn draw_pixel(buffer:&mut Vec<u32>, x:usize, y:usize, color:Color) {
+    let pixel_pos = y * WIDTH + x;
+    buffer[pixel_pos] = encode_color(color);
+}
 
-    let mut pixels = {
-        let pixel_width = win.pixel_w() as u32;
-        let pixel_height = win.pixel_h() as u32;
-        let surface_texture = SurfaceTexture::new(pixel_width, pixel_height, &win);
-        Pixels::new(WIDTH, HEIGHT, surface_texture)?
-    }; 
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
 
-    let radius = (WIDTH / 5) as i32;
-    let (cx, cy) = ((WIDTH / 2) as i32, (HEIGHT / 2) as i32);
+    let mut window = Window::new(
+        "rasterizer-rs",
+        WIDTH, HEIGHT,
+        WindowOptions::default(),
+    )?;
 
-    while app.wait() {
-        if let Err(err) = pixels.render() {
-            panic!("{}", err);
+    window.set_target_fps(60);
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for i in 0..30 {
+            draw_pixel(&mut buffer, i * WIDTH / 30  , HEIGHT / 2, Color::new(1., 1., 1.));
         }
-        for (i, pixel) in pixels.frame_mut().chunks_exact_mut(4).enumerate() {
-            let x = (i % WIDTH as usize) as i32;
-            let y = (i / WIDTH as usize) as i32;
-            
-            let rgba =
-                if sq(x - cx) + sq(y - cy) < sq(radius) {
-                    [0xff, 0xff, 0xff, 0xff]
-                } else {
-                    [0x72, 0x9f, 0xc2, 0xff]
-                };
-            
-            pixel.copy_from_slice(&rgba);
-        }
-        app::flush();
-        app::awake();
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT)?;
     }
-
     Ok(())
 }
