b6b93a06638a304b115fb2984492bd708e158a70
appbird <project.appbird@gmail.com>
2024-08-14 00:48:42 +0900

    fix the bug in draw_line

--- src/main.rs
+++ src/main.rs
@@ -55,7 +55,8 @@ fn in_range<T: PartialOrd>(begin: T, x: T, end: T) -> bool {
 }
 
 fn draw_pixel(buffer: &mut Vec<u32>, x: usize, y: usize, color: &Color) {
-    assert!(in_range(0, x, WIDTH) && in_range(0, y, HEIGHT));
+    assert!(in_range(0, x, WIDTH));
+    assert!(in_range(0, y, HEIGHT));
     let pixel_pos = y * WIDTH + x;
     buffer[pixel_pos] = encode_color(color);
 }
@@ -64,29 +65,38 @@ fn draw_pixel(buffer: &mut Vec<u32>, x: usize, y: usize, color: &Color) {
  * 点p1から点p2への線分をBresenhamの線分描画アルゴリズムによって描画する。
  */
 fn draw_line(buffer: &mut Vec<u32>, p1:&Point2, p2:&Point2, color: &Color) {
-    if p1.x > p2.x { draw_line(buffer, p2, p1, color); }
-
-    let based_on_x_axis = (p2.x - p1.x).abs() > (p2.y - p1.y).abs();
+    // どの軸に沿って線形走査を行うかを表す。`true`ならばx軸に沿って線を描き、`false`ならy軸に沿って線を描く。
+    let along_x_axis = (p2.x - p1.x).abs() > (p2.y - p1.y).abs();
+    // 走査する軸の方向に対して、開始点と到着点が真逆になっていたときはp1とp2を入れ替えておく。
+    if along_x_axis && p1.x > p2.x || !along_x_axis && p1.y > p2.y  {
+        return draw_line(buffer, p2, p1, color);
+    }
+    // |dy/dx| > 1であったときはy = xに対して対称に移動させる。点を打つ際にはちゃんと移動させた分を戻す。
     let flipped_points = (&p1.flipped(), &p2.flipped());
-    let (p1, p2) = if based_on_x_axis { (p1, p2) } else { flipped_points };
-    
+    let (p1, p2) = if along_x_axis { (p1, p2) } else { flipped_points };
+
+    // Bresenhamのアルゴリズムの実装（ただし、|dy/dx| < 1で実行できるように拡張）
     let dx = p2.x - p1.x;
     let dy = p2.y - p1.y;
+    assert!(dy.abs() < dx.abs()); // |dy/dx| < 1
+    // 誤差が大きくなった場合に描画する画素の対象を正の方向にずらすか負の方向にずらすか
+    let s = if dy > 0 { 1 } else { -1 };
     let mut y = p1.y;
     let mut err = -dx;
     for x in p1.x..p2.x {
-        err += 2 * dy;
+        err += 2 * i32::abs(dy);
         if err > 0 {
-            y += 1;
-            err -= 2*dx;
+            y += s;
+            err -= 2 * dx;
         }
-        let (x, y) = if based_on_x_axis {(x, y)} else {(y, x)};
+        // 点を打つ際にはちゃんと対称に移動させた分を戻す。
+        let (x, y) = if along_x_axis {(x, y)} else {(y, x)};
         if in_range(0, x, WIDTH as i32) && in_range(0, y, HEIGHT as i32) {
             draw_pixel(buffer, x as usize, y as usize, color)
         }
     }
-    
 }
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
 
@@ -95,10 +105,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     window.set_target_fps(24);
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        for i in 0..8 {
-            
-            //draw_line(&mut buffer, &Point2::new(50, 50), &Point2::new(300, 300), &Color::new(1.0, 1.0, 1.0));
-            draw_line(&mut buffer, &Point2::new(300, 50), &Point2::new(50, 300), &Color::new(1.0, 1.0, 1.0));
+        for i in 0..50 {
+            let theta = -PI/2.0 + (i as f64) * 2.0*PI / 50.0;
+            let r = (HEIGHT as f64) / 2.0;
+            let p1 = Point2::new((WIDTH as i32) / 2, (HEIGHT as i32) / 2);
+            let p2 = Point2::new(
+                (p1.x as f64 + r * f64::cos(theta)).floor() as i32,
+                (p1.y as f64 + r * f64::sin(theta)).floor() as i32
+            );
+            draw_line(&mut buffer, &p1, &p2, &Color::new(1., 1., 1.));
         }
         window.update_with_buffer(&buffer, WIDTH, HEIGHT)?;
     }
