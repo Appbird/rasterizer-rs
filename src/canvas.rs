6c9ad89169cb92ff9f8caecc350b25927d59cc80
appbird <project.appbird@gmail.com>
2024-09-19 15:25:22 +0900

    triangleメソッドを定義

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,3 +1,4 @@
 mod base;
 mod line;
+mod triangle;
 pub use base::Canvas;
---
+++ src/canvas/triangle.rs
@@ -0,0 +1,9 @@
+use super::Canvas;
+
+impl Canvas {
+    fn triangle(
+        &mut self,
+        points: [i32; 3]
+    ) {
+    }
+}
