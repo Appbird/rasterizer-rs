536c21068742ea5008504c08442b3222d8eaf6dc
appbird <project.appbird@gmail.com>
2024-09-21 17:25:28 +0900

    三角形描画でイテレータを切り替えたあとの挙動を修正

--- src/canvas/triangle.rs
+++ src/canvas/triangle.rs
@@ -1,6 +1,6 @@
 use crate::util::{ClosedInterval, Color, Point2};
 
-use super::{bresenham::{BresenhamLine, BresenhamLineIter}, Canvas};
+use super::{bresenham::BresenhamLine, Canvas};
 
 impl Canvas {
     fn triangle(
@@ -20,6 +20,7 @@ impl Canvas {
             let p1 =
                 if let Some(x) = iter1[iter1_idx].next() { x } else {
                     iter1_idx += 1;
+                    iter1[iter1_idx].skip_to_next_y();
                     iter1[iter1_idx].next().unwrap()
                 };
             let p2 = if let Some(x) = iter2.next() { x } else { break; };
@@ -30,7 +31,7 @@ impl Canvas {
             let after = ClosedInterval::between(edge1.x, edge2.x);
             let draw_interval = before.or(after);
             for x in draw_interval {
-                
+                self.draw_pixel(&Point2::new(x, y), color);
             }
             
         }
