5789020a9485e388f7942b093ca5ef277278b6ad
appbird <project.appbird@gmail.com>
2024-09-29 19:40:14 +0900

    mat4x4の宣言

--- src/util.rs
+++ src/util.rs
@@ -1,10 +1,12 @@
 mod point2;
 mod vec4;
 mod interval;
+mod mat4x4;
 
 pub use point2::*;
 pub use vec4::*;
 pub use interval::*;
+pub use mat4x4::*;
 
 pub fn in_range<T: PartialOrd>(begin: T, x: T, end: T) -> bool {
     begin <= x && x < end
---
+++ src/util/mat4x4.rs
@@ -0,0 +1,3 @@
+struct Mat4x4 {
+    
+}
