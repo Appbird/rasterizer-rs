54811135732b02ee87ae1afdc34e5526c7532751
appbird <project.appbird@gmail.com>
2024-09-30 23:57:37 +0900

    カメラの深度や画角を調節できるようにフィールドを定義

--- src/camera.rs
+++ src/camera.rs
@@ -1,13 +1,30 @@
 use crate::{
     canvas::Canvas,
-    util::{Color, Point2, Vec4Project, Vec4Screen}
+    util::{Color, Mat4x4, Point2, Vec4Project, Vec4Screen}
 };
+use std::f64::consts::PI;
 
 pub struct Camera {
+    near:f64,
+    far:f64,
+    width:f64,
+    height:f64
 }
 
 impl Camera {
-    pub fn new() -> Camera { Camera{} }
+    pub fn new(aspect: f64) -> Camera {
+        let near = 0.1;
+        let width = 2. * near * f64::tan(PI /4.);
+        Camera{
+            near,
+            far: 1000.,
+            width,
+            height: width * aspect,
+        }
+    }
+    pub fn view_conversion(&self) -> Mat4x4 {
+        
+    }
     pub fn draw_point(&mut self, canvas:&mut Canvas, p1:&Vec4Project, color:&Color) {
         let p1 = self.transform_into_screen(canvas.size(), &p1).to_point2();
         canvas.draw_point(&p1, color);
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod util;
 mod canvas;
 mod camera;
 mod world;
+mod actor;
 
 use std::{thread, time::{self, Instant}};
 
@@ -13,8 +14,9 @@ use util::{Color, Throwable};
 fn main() -> Throwable<()> {
     let w: usize = 640;
     let h: usize = 480;
+    let aspect = (h as f64) / (w as f64);
     let mut canvas = Canvas::new(w, h)?;
-    let mut camera = Camera::new();
+    let mut camera = Camera::new(aspect);
     
     let start_instant = Instant::now();
     let mut previous_instant = start_instant;
