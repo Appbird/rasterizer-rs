29f22234c0bc2cc107ec8b745c0102ef72588c84
appbird <project.appbird@gmail.com>
2024-10-01 12:08:33 +0900

    snapshot関数を実装

--- src/camera.rs
+++ src/camera.rs
@@ -1,6 +1,5 @@
 use crate::{
-    canvas::Canvas,
-    util::{Color, Mat4x4, Point2, Vec4Project, Vec4Screen}
+    actor::Actor, canvas::Canvas, util::{Color, Mat4x4, Point2, Vec4, Vec4Project, Vec4Screen}
 };
 use std::f64::consts::PI;
 
@@ -22,8 +21,33 @@ impl Camera {
             height: width * aspect,
         }
     }
+    pub fn snapshot(&mut self, canvas:&mut Canvas, actors:&Vec<Actor>) {
+        let perspective = self.view_conversion();
+        let view = Mat4x4::identity();
+        let pv = perspective*view;
+
+        for actor in actors {
+            let model = actor.model_conversion();
+            let pvm = &pv * &model;
+            let projected:[Vec4Project; 3] = [
+                Vec4Project(&pvm * &actor.vertices[0]),
+                Vec4Project(&pvm * &actor.vertices[1]),
+                Vec4Project(&pvm * &actor.vertices[2]),
+            ];
+            self.draw_triangle(canvas, &projected[0], &projected[1], &projected[2], &actor.color);
+        }
+    }
     pub fn view_conversion(&self) -> Mat4x4 {
-        
+        let n = self.near;
+        let f = self.far;
+        let w = self.width;
+        let h = self.height;
+        Mat4x4::from_array([
+            [ n / w,    0.,     0.,     0.  ],
+            [ 0.,       n / h,  0.,     0.  ],
+            [ 0.,       0.,     (f+n)/(f-n), -2.*(f*n)/(f - n)],
+            [ 0.,       0.,     1.,     0.  ],
+        ])
     }
     pub fn draw_point(&mut self, canvas:&mut Canvas, p1:&Vec4Project, color:&Color) {
         let p1 = self.transform_into_screen(canvas.size(), &p1).to_point2();
