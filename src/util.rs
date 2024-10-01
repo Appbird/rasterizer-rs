b9ead93ee48c12300e826a409ed3197525b26937
appbird <project.appbird@gmail.com>
2024-10-01 21:19:17 +0900

    utilに追加

--- src/util.rs
+++ src/util.rs
@@ -2,6 +2,7 @@ mod point2;
 mod vec4;
 mod interval;
 mod mat4x4;
+mod debug;
 
 pub use point2::*;
 pub use vec4::*;
@@ -11,4 +12,5 @@ pub use mat4x4::*;
 pub fn in_range<T: PartialOrd>(begin: T, x: T, end: T) -> bool {
     begin <= x && x < end
 }
+
 pub type Throwable<T> = Result<T, Box<dyn std::error::Error>>;
