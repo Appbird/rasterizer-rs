3136e44fd69d34984b637444e7657cf73c030e40
appbird <project.appbird@gmail.com>
2024-10-01 12:07:27 +0900

    Actorの実装

--- src/actor.rs
+++ src/actor.rs
@@ -2,13 +2,14 @@ use crate::util::{Mat4x4, Vec4};
 
 pub struct Actor {
     pub vertices:[Vec4; 3],
-    position:Vec4,
-    axis:Vec4,
-    theta:f64,
+    pub position:Vec4,
+    pub axis:Vec4,
+    pub theta:f64,
+    pub color: Vec4
 }
 
 impl Actor {
-    fn model_conversion(&self) -> Mat4x4 {
+    pub fn model_conversion(&self) -> Mat4x4 {
         Mat4x4::translate(&self.position)
         * Mat4x4::rotation(&self.axis, self.theta)
     }
